a3e4b834052eb9da8d89135bc84f72bf30afb086
simjihoons <wlgnsgit@gmail.com>
2022-05-16 19:45:22 +0900

    mongoose 기본설정

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://admin:abcd1234@sim-s-shop.2epri.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("MongoDB 연결 성공"))
+  .catch((err) => console.log(err));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}~!`);
