72a8b2dfcd5c2cedebf86204b819699f9ba8caa4
simjihoons <wlgnsgit@gmail.com>
2022-05-16 20:08:52 +0900

    User 모델

--- models/User.js
+++ models/User.js
@@ -4,15 +4,21 @@ const userSchema = mongoose.Schema({
   name: {
     type: String,
     maxLength: 50,
+    trim: true,
+    required: true,
   },
   email: {
     type: String,
     trim: true,
     unique: 1,
+    required: true,
   },
   password: {
     type: String,
     minlength: 5,
+    trim: true,
+    lowercase: true,
+    required: true,
   },
   role: {
     type: Number,
