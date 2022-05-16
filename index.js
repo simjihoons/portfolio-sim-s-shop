9ffdadb9ce2b825a4965250a44a57f6827fb6fcb
simjihoons <wlgnsgit@gmail.com>
2022-05-16 20:20:41 +0900

    비밀정보 관리 수정

--- index.js
+++ index.js
@@ -2,13 +2,33 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const { User } = require("./models/User");
+
+const bodyParser = require("body-parser");
+const config = require("./config/key");
+
+//application/x-www-form-urlencoded 파일을 분석
+app.use(bodyParser.urlencoded({ extended: true }));
+//application/json 파일을 분석
+app.use(bodyParser.json());
+
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://admin:abcd1234@sim-s-shop.2epri.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-  )
+  .connect(config.mongoURI)
   .then(() => console.log("MongoDB 연결 성공"))
   .catch((err) => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello!!"));
+
+app.post("/register", (req, res) => {
+  //회원가입 정보를 DB로 보냄
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}~!`);
