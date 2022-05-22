2bdb5bf9faa90543f7b298513dc277c1784a21fe
simjihoons <wlgnsgit@gmail.com>
2022-05-23 00:57:05 +0900

    redux

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,28 @@ import "./index.css";
 import App from "./App";
 import "antd/dist/antd.min.css";
 
+//redux 연결
+import { Provider } from "react-redux";
+import { applyMiddleware, legacy_createStore as createStore } from "redux";
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+import Reducer from "./_reducers";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const createStoreWithMiddleware = applyMiddleware(
+  promiseMiddleware,
+  ReduxThunk
+)(createStore);
+
 root.render(
-  <React.StrictMode>
+  <Provider
+    store={createStoreWithMiddleware(
+      Reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+  >
     <App />
-  </React.StrictMode>
+  </Provider>
 );
